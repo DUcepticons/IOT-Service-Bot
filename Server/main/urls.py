4abd971adb114ee27f5ecee1ff7ff4b9fdeee8c8
Niloy Irtisam <niloyirtisam@hotmail.com>
2020-01-13 20:40:14 +0600

    index temp

--- Server/main/urls.py
+++ Server/main/urls.py
@@ -1,4 +1,4 @@
-"""Server URL Configuration
+"""salineweb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
+app_name= 'main'
 urlpatterns = [
-	path('',include('main.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.homepage, name='homepage'),
+    
+    
 ]
