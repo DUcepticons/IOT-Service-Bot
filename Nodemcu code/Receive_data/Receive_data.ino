3eacce4281f947c0e54fbf29dc5e8bb3911fbb6f
BiTechX <52671690+BiTechX@users.noreply.github.com>
2020-01-18 01:47:56 +0600

    hello

--- Nodemcu code/Receive_data/Receive_data.ino
+++ Nodemcu code/Receive_data/Receive_data.ino
@@ -12,19 +12,94 @@
 
 #include "ESP_MICRO.h"
 
-#define LED 4
 
+
+//motorPins
+const int motorPin1 = 14,motorPin2 = 12;        //right motor
+const int motorPin3 = 13,motorPin4 = 15;       //left motor
+
+int rotateDelay=100, forwardDelay=1000;
 void setup(){
   Serial.begin(9600);
-  start("Niloy","1q2w3e4r5t6Y"); // Wifi details connec to
-
-  pinMode(LED,OUTPUT);
+  start("Epitapher Thanda","Qawsedrf"); // Wifi details connec to
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 }
 
 void loop(){
   waitUntilNewReq();  //Waits until a new request from python come
 
-   Serial.print(getPath());
+  Serial.print(getPath());
+  if (getPath()=="/nn")
+  {
+    goForward(forwardDelay);
+  }
+
+  else if (getPath()=="/ww")
+  {
+     rotateLeft(rotateDelay);
+     goForward(forwardDelay);
+  }
+
+
+  else if (getPath()=="/ee")
+  {
+     rotateRight(rotateDelay);
+     goForward(1000);
+  }
+  
   returnThisStr("GotIt");
+  goForward(forwardDelay);
+
 
 }
+
+
+void goForward(int delayTime)
+{
+  analogWrite(motorPin1, 255);
+  analogWrite(motorPin2 , 0);
+  analogWrite(motorPin3,255);
+  analogWrite(motorPin4, 0);
+  delay(delayTime);
+  
+
+}
+
+void rotateLeft(int delayTime)
+{
+  analogWrite(motorPin1, 200);
+  analogWrite(motorPin2 , 0);
+  analogWrite(motorPin3, 0);
+  analogWrite(motorPin4, 200);
+  delay(delayTime);
+  
+
+}
+
+void rotateRight(int delayTime)
+{
+  analogWrite(motorPin1, 0);
+  analogWrite(motorPin2 , 200);
+  analogWrite(motorPin3, 200);
+  analogWrite(motorPin4, 0);
+  delay(delayTime);
+  
+
+}
+
+
+void brake(int delayTime)
+{
+  analogWrite(motorPin1, 0);
+  analogWrite(motorPin2 , 0);
+  analogWrite(motorPin3, 0);
+  analogWrite(motorPin4, 0);
+  delay(delayTime);
+
+}
+
+
+
